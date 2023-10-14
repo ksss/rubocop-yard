ae674c348e8aa5bd54c74ac19f721b7b83c8ee96
ksss <co000ri@gmail.com>
2023-10-14 17:50:12 +0900

    Check autocorrect by git diff

---
+++ smoke/generated/mismatch_name_correct.rb
@@ -0,0 +1,27 @@
+class Foo
+  # @param [void] bar
+  # @param
+  # @param aaa
+  # @param [void]
+  # @option opt aaa [void]
+  # @option opts aaa
+  # @option opts aaa [void]
+  # @param [void]
+  # @param (see #route_docs)
+  # @param [Hash<Symbol=>Object] config Hash containing optional configuration
+  # @return [void]
+  # @return [void] fooo
+  def foo(bar, opts = {})
+  end
+
+  # @param [String] strings
+  # @param [Object] opts
+  # @param [Object] a
+  # @param [Array<Object>] rest
+  # @param [Hash{Symbol => Object}] kw
+  def bar(strings, opts = {}, a = nil, *rest, **kw)
+  end
+
+  def empty_doc(arg)
+  end
+end
